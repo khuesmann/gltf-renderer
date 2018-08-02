5b5b0137abcd09bc502640b5486587376c2115b3
Karim <karimhuesmann@me.com>
2018-08-02 11:36:15 +0200

    initial commit

---
+++ app/src/main/java/org/renderer/gltf/gltf_renderer/Renderer.java
@@ -0,0 +1,13 @@
+package org.renderer.gltf.gltf_renderer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Renderer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renderer);
+    }
+}
