7197544b37590998bc9e710df496ed30533666b4
Karim <karimhuesmann@me.com>
2018-08-02 14:26:37 +0200

    working example

--- app/src/main/java/org/renderer/gltf/gltf_renderer/Renderer.java
+++ app/src/main/java/org/renderer/gltf/gltf_renderer/Renderer.java
@@ -1,13 +1,48 @@
 package org.renderer.gltf.gltf_renderer;
 
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+
+import org.renderer.gltf.gltf_renderer.SampleGLTFView;
+
+import org.renderer.gltf.gltf_renderer.R;
 
 public class Renderer extends AppCompatActivity {
+    private SampleGLTFView glTFView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_renderer);
+        glTFView = (SampleGLTFView) findViewById(R.id.gltf_view);
+        init();
+    }
+
+    private void init() {
+        glTFView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                glTFView.initRenderThread(surface, width, height);
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                glTFView.releaseResources();
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            }
+        });
+        glTFView.setVisibility(View.VISIBLE);
     }
 }
